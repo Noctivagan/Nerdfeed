33771241638cd474bac688828f5680c05deb4403
Tom Anderson <silverwing@noctivagan.com>
2014-10-17 15:58:25 -0400

    Initial Commit

---
+++ Nerdfeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Nerdfeed
+//
+//  Created by Tom Anderson on 10/17/14.
+//  Copyright (c) 2014 C-leveled. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
